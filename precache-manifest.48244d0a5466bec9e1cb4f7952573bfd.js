07bf3f79b3c71a9e4bbf8b780284b9c0c0690062
YudinSergey <serg93.12@mail.ru>
2020-03-29 02:46:44 +0300

    Updates

---
+++ precache-manifest.48244d0a5466bec9e1cb4f7952573bfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0caac11228f86db055c340584c7269",
+    "url": "/filmslist/index.html"
+  },
+  {
+    "revision": "8276a9c1eb7d801ee601",
+    "url": "/filmslist/static/css/main.c7294b11.chunk.css"
+  },
+  {
+    "revision": "7c796ba4c70424802447",
+    "url": "/filmslist/static/js/2.7a92efcc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/filmslist/static/js/2.7a92efcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8276a9c1eb7d801ee601",
+    "url": "/filmslist/static/js/main.e26869b6.chunk.js"
+  },
+  {
+    "revision": "540b983bfafba84bc71d",
+    "url": "/filmslist/static/js/runtime-main.31684e42.js"
+  }
+]);
